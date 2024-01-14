aa490fd8f145796bd5535859573d0f28a0d583e4
Nikolay Panyukov <panyukovnn@gmail.com>
2024-01-15 00:25:52 +0300

    fix

--- src/main/kotlin/ru/panyukovnn/sbaserver/config/SecurityConfig.kt
+++ src/main/kotlin/ru/panyukovnn/sbaserver/config/SecurityConfig.kt
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 import java.util.*
 
 @Configuration
+@EnableWebFluxSecurity
 class SecurityConfig {
 
     @Bean
